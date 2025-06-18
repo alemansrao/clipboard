d78aeec237bc2f20730238abf29be719f4d8351d
alemansrao <sharathrao99@gmail.com>
2025-06-18 13:30:18 +0530

    Refactor NavbarComponent: improve structure and readability

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-import { Navbar, NavbarBrand, NavbarContent, Divider, NavbarItem, Link } from "@heroui/react"
+import { Navbar, NavbarBrand, NavbarContent, Divider, NavbarItem, Link, Button } from "@heroui/react"
 import { FaClipboard, FaPlusCircle } from "react-icons/fa"
 import { ThemeSwitch } from "@/components/theme-switch"
+import { useSession } from "next-auth/react"
+
 const NavbarComponent = () => {
+  const { status } = useSession();
   return (
     <>
       <Navbar>
@@ -17,6 +20,16 @@ const NavbarComponent = () => {
           <NavbarItem className="justify-center items-center flex gap-3">
             <ThemeSwitch />
           </NavbarItem>
+          {/* Show logout only if authenticated */}
+          {status === "authenticated" && (
+            <NavbarItem className="justify-center items-center flex gap-3">
+              <Link href='/logout'>
+                <Button color="white" variant="ghost" className="border-2 rounded-none border-gray-300 text-primary-900">
+                  Logout
+                </Button>
+              </Link>
+            </NavbarItem>
+          )}
         </NavbarContent>
       </Navbar>
       <Divider />
