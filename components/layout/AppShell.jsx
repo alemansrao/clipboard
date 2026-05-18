da40f784ef1f6a59c2a9a08e9ce97eaf36569112
alemansrao <sharathrao99@gmail.com>
2026-05-18 12:56:49 +0530

    fixing

--- components/layout/AppShell.jsx
+++ components/layout/AppShell.jsx
@@ -4,7 +4,7 @@ export default function AppShell({ children, session }) {
   return (
     <div className="min-h-screen bg-zinc-50 text-zinc-950 dark:bg-zinc-950 dark:text-zinc-50">
       <Navbar session={session} />
-      <main className="mx-auto w-full max-w-[90%] px-4 pb-16 pt-8 sm:px-6 lg:px-8">{children}</main>
+      <main className="mx-auto w-full max-w-6xl px-4 pb-16 pt-8 sm:px-6 lg:px-8">{children}</main>
     </div>
   );
 }
